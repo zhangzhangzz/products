1eb205619ce139d4864bcfda34dd3362ad142e32
shanpao <shanpao@shanpao.net>
2019-04-18 14:16:34 +0800

    chenxu

--- app/Http/Controllers/Admin/Goods/CategoryController.php
+++ app/Http/Controllers/Admin/Goods/CategoryController.php
@@ -99,8 +99,10 @@ class CategoryController extends Controller{
         $category = $request->all();
         unset($category["_token"]);
         $images = $_FILES["images"];
-        $images_url = ImagesUrl($images);
-        $category["images"] = $images_url;
+        if(!empty($images["tmp_name"])){
+            $images_url = ImagesUrl($images);
+            $category["images"] = $images_url;
+        }
         $bool = DB::table("goods_type")->where("id",$category["id"])->update($category);
         if($bool){
             return redirect("admin/category/index")->with("message","修改成功");
